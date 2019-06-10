b4d69df5b8948209e0a69358b161180d24442a75
Alastair Codd <alastaircodd@hotmail.co.uk>
2019-06-10 22:56:13 +0100

    update TileContainer

--- Core/tileContainer.py
+++ Core/tileContainer.py
@@ -1,21 +1,28 @@
 from typing import List
+from Core.cavernaEnums import TileDirectionEnum, TileTypeEnum
+from Core.baseEffect import BaseEffect
+from Core.baseTile import BaseTile
 
 class TileContainer(object):
 	
-	def __init__(self, height=):
-		self._tiles: List[BaseTile] = []
+	def __init__(self, height=6, width=8):
+		if height < 0:
+			raise ValueError("height must be greater than 0")
+		if width < 0:
+			raise ValueError("width must be greater than 0")
+		self._tiles: List[BaseTile] = [x for x in range(height*width)]
 		self._twinTiles = [
-			meadowFieldTwin,
-			cavernTunnelTwin,
-			cavernCavernTwin,
-			pastureTwin,
-			oreMineDeepTunnelTwin ]
+			TileTypeEnum.meadowFieldTwin,
+			TileTypeEnum.cavernTunnelTwin,
+			TileTypeEnum.cavernCavernTwin,
+			TileTypeEnum.pastureTwin,
+			TileTypeEnum.oreMineDeepTunnelTwin ]
 		
 	def GetTiles(self) -> List[BaseTile]:
 		return self._tiles
 		
-	def GetEffects(self) -> List[baseEffect]:
-		effects = map(lambda tile: tile.GetEffects, self._tiles)
+	def GetEffects(self) -> List[BaseEffect]:
+		effects = map(lambda tile: tile.GetEffects(), self._tiles)
 		return list(effects)
 		
 	def GetEffectsOfType(self, type) -> List[baseEffect]:
@@ -32,6 +39,10 @@ class TileContainer(object):
 		if tile in self._twinTiles and direction is None:
 			raise ValueError("direct cannot be null if tile is a twin tile")
 		
-		
 	def GetAdjacentTile(self, location: int, direction: TileDirectionEnum) -> int:
-		directionOffset = 
+		directionOffset = {
+			TileDirectionEnum.up: -8,
+			TileDirectionEnum.down: 8,
+			TileDirectionEnum.left: -1,
+			TileDirectionEnum.right: 1 }
+		raise NotImplementedException()
