4ccc1564ac14316501803c71c1cde37b631f4319
Alastair Codd <alastaircodd@hotmail.co.uk>
2019-06-11 22:43:12 +0100

    Implement TileContainerDefault

--- Common/Services/TileContainerDefault.py
+++ Common/Services/TileContainerDefault.py
@@ -1,15 +1,21 @@
-from Common.Entities.dwarf import Dwarf
+from typing import Dict
+from Core.cavernaEnums import TileTypeEnum
 
 class TileContainerDefault(object):
 
-	def GetInitialTiles(self) -> :
-		underground
-cavern
-tunnel
-deepTunnel
-cavernTunnelTwin
-cavernCavernTwin
-furnishedCavern
-furnishedDwelling
-oreMineDeepTunnelTwin
-rubyMine
+	def AssignInitialTiles(self, currentTiles: Dict[int, TileTypeEnum]) -> Dict[int, TileTypeEnum]:
+		for x in range(48):
+			if (x < 8 or x > 40 or x % 8 == 0 or x % 8 == 7):
+				currentTiles[x] = TileTypeEnum.unavailable
+			elif x % 8 < 4:
+				currentTiles[x] = TileTypeEnum.forest
+			else:
+				currentTiles[x] = TileTypeEnum.underground
+
+		currentTiles = self._specificIntialTileOverrides( currentTiles )
+		return currentTiles
+		
+	def _specificIntialTileOverrides(self, currentTiles: Dict[int, TileTypeEnum]) -> Dict[int, TileTypeEnum]:
+		currentTiles[36] = TileTypeEnum.furnishedDwelling
+		currentTiles[28] = TileTypeEnum.cavern
+		return currentTiles
