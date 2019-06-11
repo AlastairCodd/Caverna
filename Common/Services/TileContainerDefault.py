a471be9c55893bd66cbc2ee3e395f1c3e6bbdbb4
Alastair Codd <Alastair.Codd@abdynamics.com>
2019-06-11 16:29:10 +0100

    Add TileContainerDefault

---
+++ Common/Services/TileContainerDefault.py
@@ -0,0 +1,15 @@
+from Common.Entities.dwarf import Dwarf
+
+class TileContainerDefault(object):
+
+	def GetInitialTiles(self) -> :
+		underground
+cavern
+tunnel
+deepTunnel
+cavernTunnelTwin
+cavernCavernTwin
+furnishedCavern
+furnishedDwelling
+oreMineDeepTunnelTwin
+rubyMine
