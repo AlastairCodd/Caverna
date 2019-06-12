5c206d3bdb684eb1354b62f4cd624392e53e9968
Alastair Codd <alastaircodd@hotmail.co.uk>
2019-06-12 07:58:42 +0100

    Add Board Effects

--- BuisnessLogic/Effects/boardEffects.py
+++ BuisnessLogic/Effects/boardEffects.py
@@ -1,14 +1,31 @@
+from typing import Dict, List
 from player import Player
 from Core.baseEffect import BaseEffect
+from Core.cavernaEnums import TileTypeEnum
+from Common.TileTwinDefault import TileTwinDefault
 
 class BaseBoardEffect(BaseEffect):
-	def Invoke(self, player: Player) -> bool:
+	def Invoke(self, source: Dict[TileTypeEnum, List[TileTypeEnum]]) -> Dict[TileTypeEnum, List[TileTypeEnum]]:
 		raise NotImplementedException("base population effect class")
 	
 class FurnishTunnelsEffect(BaseBoardEffect):
-	def Invoke(self, player: Player) -> bool:
-		raise NotImplementedException()
+	def Invoke(self, source: Dict[TileTypeEnum, List[TileTypeEnum]]) -> Dict[TileTypeEnum, List[TileTypeEnum]]:
+		if source is None:
+			raise ValueError()
+		
+		source[TileTypeEnum.furnishedCavern].extend([TileTypeEnum.tunnel, TileTypeEnum.deepTunnel]
+		source[TileTypeEnum.furnishedDwelling].extend([TileTypeEnum.tunnel, TileTypeEnum.deepTunnel]
+		return source
 		
 class TwinTilesOverhangEffect(BaseBoardEffect):
-	def Invoke(self, player: Player) -> bool:
-		raise NotImplementedException()
+	def Invoke(self, source: Dict[TileTypeEnum, List[TileTypeEnum]]) -> Dict[TileTypeEnum, List[TileTypeEnum]]:
+		if source is None:
+			raise ValueError()
+			
+		twinDefault = TileTwinDefault.TileTwinDefault()
+		twinTiles = twinDefault.Assign( [] )
+		
+		for x in source if x in twinTiles:
+			source[x].add( TileTypeEnum.unavailable )
+			
+		return source
