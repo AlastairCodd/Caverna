f255d619ec6e6bbf1fc22811d6dc2428ba5507bc
Alastair Codd <Alastair.Codd@abdynamics.com>
2019-06-18 13:53:46 +0100

    Add return types to Caverna Env

--- Common/Environment/CavernaEnv.py
+++ Common/Environment/CavernaEnv.py
@@ -1,12 +1,14 @@
+from typing import Tuple, Dict
 from gym import Env
+from numpy import array
 
 class CavernaEnv(Env):
-	
-	def reset(self):
-		raise NotImplementedError()
-        
-    def render(self):
+    
+    def reset(self) -> array:
         raise NotImplementedError()
         
-    def step(self, action):
-        raise NotImplementedError()
+    def step(self, action: array) -> Tuple[array, float, bool, Dict]:
+        if action is None:
+            raise ValueError("action")
+            
+        
