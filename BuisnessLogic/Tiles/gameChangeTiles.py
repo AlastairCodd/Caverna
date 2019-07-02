151f094febc0c4dfca76c56beb8b6d74e3779312
Alastair Codd <alastaircodd@hotmail.co.uk>
2019-07-02 08:37:14 +0100

    Move animal storage tiles

--- BuisnessLogic/Tiles/gameChangeTiles.py
+++ BuisnessLogic/Tiles/gameChangeTiles.py
@@ -2,35 +2,6 @@ from Core.cavernaEnums import ResourceTypeEnum
 from BuisnessLogic.Effects import populationEffects, animalStorageEffect
 from Core.baseTile import BaseTile
 
-class CuddleRoomTile(BaseTile):
-	def __init__(self):
-		self._name = "Cuddle Room"
-		self._id = 6
-		self._isDwelling = False
-		self._basePoints = 2
-		self._cost = { ResourceTypeEnum.wood: 1 }
-		self._effect = [animalStorageEffect.ConditionalStorageEffect( Per, ResourceTypeEnum.sheep, dwarves )]
-			
-class BreakfastRoomTile(BaseTile):
-	def __init__(self):
-		self._name = "Breakfast Room"
-		self._id = 7
-		self._isDwelling = False
-		self._basePoints = 0
-		self._cost = { ResourceTypeEnum.wood: 1 }
-		self._effect = [animalStorageEffect.IncreasePopulationCapEffect( 3, ResourceTypeEnum.cow )]
-			
-class StubbleRoomTile(BaseTile):
-	def __init__(self):
-		self._name = "Stubble Room"
-		self._id = 8
-		self._isDwelling = False
-		self._basePoints = 1
-		self._cost = {
-			ResourceTypeEnum.wood: 1,
-			ResourceTypeEnum.ore: 1 }
-		self._effect = [animalStorageEffect.IncreasePopulationCapForTileTypeEffect( 1, TileTypeEnum.field )]
-			
 class WorkRoomTile(BaseTile):
 	def __init__(self):
 		self._name = "Work Room"
