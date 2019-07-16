078317d7f00bcaabb0fddb176aab139f8a43e4f7
Alastair Codd <Alastair.Codd@abdynamics.com>
2019-07-16 13:32:46 +0100

    Start working on Reset Action

--- Common/Environment/CavernaEnv.py
+++ Common/Environment/CavernaEnv.py
@@ -3,12 +3,27 @@ from gym import Env
 from numpy import array
 
 class CavernaEnv(Env):
+    """Environment for running caverna games"""
+    def __init__(self, numberOfPlayers: int = 7):
+        """Ctor
+        
+        Params: numberOfPlayers: int. Must be between 1 and 7 (inclusive)."""
+        if numberOfPlayers < 1: raise IndexError("numberOfPlayers")
+        if numberOfPlayers > 7: raise IndexError("numberOfPlayers")
+        self._numberOfPlayers = numberOfPlayers
     
     def reset(self) -> array:
         raise NotImplementedError()
         
-    def step(self, action: array) -> Tuple[array, float, bool, Dict]:
-        if action is None:
-            raise ValueError("action")
-            
+        self._players = []
+        
+        for x in range(self._numberOfPlayers):
+            self._players[x] = player.Player( x, x )
+        
+    def step(self, action: ActionService) -> Tuple[array, float, bool, Dict]:
+        """Takes an action service (either the output of a network or a delayed decision maker)
+        and applies the actions to get the next state"""
+        if action is None: raise ValueError("action")
+        
+        
         
