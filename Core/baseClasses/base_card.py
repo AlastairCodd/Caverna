af10e96deb63c6447bd758a1ed68e767f3c1a7da
Alastair Codd <alastaircodd@hotmail.co.uk>
2019-07-19 08:29:01 +0100

    Start implementing BaseCard.activate_card method
    * Remove unnecessary refill_action
    * Change scope of class fields

--- Core/baseClasses/base_card.py
+++ Core/baseClasses/base_card.py
@@ -4,18 +4,22 @@ from core.baseClasses.base_action import BaseAction
 from common.entities.dwarf import Dwarf
 
 class BaseCard(object):
-    _name: str = "Uninitialised"
-    _id: int = -1
-    _level: int = -1
-    _actions: Iterable[BaseAction] = []
-    _actionCombinationType: ActionCombinationEnum = ActionCombinationEnum.EitherOr
-    _currentItems: Dict[ResourceTypeEnum, int] = {}
+
+    def __init__(self):
+        self._name: str = "Uninitialised"
+        self._id: int = -1
+        self._level: int = -1
+        self._actions: Iterable[BaseAction] = []
+        self._isActive = False
     
-    def refill_action(self) -> Dict[ResourceTypeEnum, int]:
-        raise NotImplementedError("abstract base card class")
-        
     def activate_card(
             self, 
             player,
             dwarf: Dwarf ) -> bool:
-        return False
+        if player is None: raise ValueError("player")    
+        if dwarf is None: raise ValueError("dwarf")    
+        if dwarf.get_is_active: raise ValueError("dwarf cannot already be active")
+        
+        for action in self._actions:
+            action.invoke(player, self)
+        dwarf.set_active(self)
