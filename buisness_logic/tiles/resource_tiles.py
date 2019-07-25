79beccea306ab541423ebf37383e5b93117325ab
Alastair Codd <alastaircodd@hotmail.co.uk>
2019-07-25 08:23:06 +0100

    Add Additional Resource Tiles

--- buisness_logic/tiles/resource_tiles.py
+++ buisness_logic/tiles/resource_tiles.py
@@ -8,3 +8,30 @@ class MinerTile(BaseTile):
         self._basePoints = 3
         self._cost = { ResourceTypeEnum.wood: 1, ResourceTypeEnum.stone: 1 }
         self._effect = [ resourceEffects.ReceiveConditional( {ResourceTypeEnum.donkey: 1}, {ResourceTypeEnum.ore: 1} ) ]
+
+class WoodSupplierTile(BaseTile):
+    def __init__(self):
+        self._name = "Wood Supplier"
+        self._id = 18
+        self._isDwelling = False
+        self._basePoints = 2
+        self._cost = { ResourceTypeEnum.stone: 1 }
+        self._effect = [ resourceEffects.ReceiveForTurns( {ResourceTypeEnum.wood: 1 }, 7 ) ]
+
+class StoneSupplierTile(BaseTile):
+    def __init__(self):
+        self._name = "Stone Supplier"
+        self._id = 19
+        self._isDwelling = False
+        self._basePoints = 1
+        self._cost = { ResourceTypeEnum.wood: 1 }
+        self._effect = [ resourceEffects.ReceiveForTurns( {ResourceTypeEnum.stone: 1 }, 5 ) ]
+
+class RubySupplierTile(BaseTile):
+    def __init__(self):
+        self._name = "Ruby Supplier"
+        self._id = 20
+        self._isDwelling = False
+        self._basePoints = 2
+        self._cost = { ResourceTypeEnum.stone: 2, ResourceTypeEnum.wood: 2 }
+        self._effect = [ resourceEffects.ReceiveForTurns( {ResourceTypeEnum.ruby: 1 }, 4 ) ]
