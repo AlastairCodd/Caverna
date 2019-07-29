dbfafd513dfccb46e509feff39329f790e771fbf
Alastair Codd <Alastair.Codd@abdynamics.com>
2019-07-29 16:07:37 +0100

    Add Constructor to Base Tile

--- Core/baseClasses/base_tile.py
+++ Core/baseClasses/base_tile.py
@@ -1,10 +1,22 @@
-from typing import Dict
+from typing import Dict, List
 from core.enums.caverna_enums import ResourceTypeEnum
+from core.baseClasses.base_effect import BaseEffect
+
 
 class BaseTile(object):
-    _name: str = "Uninitialised"
-    _id: int = -1
-    _isDwelling = False
-    _cost: Dict[ResourceTypeEnum, int] = {}
-    _basePoints: int = 0
-    _effect = None
+
+    def __init__(
+            self,
+            name: str,
+            tile_id: int,
+            is_dwelling: bool = False,
+            base_points: int = 0,
+            cost: Dict[ResourceTypeEnum, int] = {},
+            effects: List[BaseEffect] = []):
+        """Constructor for base Tile class. """
+        self._name = name
+        self._id = tile_id
+        self._isDwelling = is_dwelling
+        self._basePoints = base_points
+        self._cost = cost
+        self._effects = effects
