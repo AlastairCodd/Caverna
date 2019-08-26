724d2842497c9df05ddce77e438eda612ee3afdf
Alastair Codd <alastaircodd@hotmail.co.uk>
2019-08-26 17:21:12 +0100

    Move mocks into solo package.

---
+++ automated_tests/mocks/mock_player.py
@@ -0,0 +1,46 @@
+from typing import Dict, List, Union
+
+from common.entities.player import Player
+from common.entities.weapon import Weapon
+from core.baseClasses.base_tile import BaseTile
+from core.enums.caverna_enums import ResourceTypeEnum
+
+
+class MockPlayer(Player):
+    def __init__(self, tiles: List[BaseTile] = None):
+        Player.__init__(self, 0, 0)
+        if tiles is None:
+            tiles = []
+        self._mock_tiles = tiles
+
+    @property
+    def tiles(self) -> List[BaseTile]:
+        if any(self._mock_tiles):
+            return self._mock_tiles
+        return super().tiles
+
+    def get_player_choice(self, action):
+        pass
+
+    def get_player_choice_market_action(
+            self,
+            possible_items_and_costs: Dict[ResourceTypeEnum, int]) \
+            -> List[ResourceTypeEnum]:
+        pass
+
+    def get_player_choice_weapon_level(self) -> int:
+        pass
+
+    def get_player_choice_breed_animals(
+            self,
+            animals_which_can_reproduce: List[ResourceTypeEnum],
+            possible_number_of_animals_to_reproduce: int) \
+            -> List[ResourceTypeEnum]:
+        pass
+
+    def get_player_choice_discount(
+            self,
+            possible_prices: List[Dict[ResourceTypeEnum, int]],
+            target: Union[BaseTile, Weapon]) \
+            -> Dict[ResourceTypeEnum, int]:
+        pass
