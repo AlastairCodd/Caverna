7ab1e69bf2392d1114f70aac877745c6f5acbd65
Alastair Codd <alastaircodd@hotmail.co.uk>
2019-08-27 23:19:35 +0100

    Fix signature on purchase effects
    * Change superclass ctor call to use direct invoke
    * Correct effects in builder tile

--- buisness_logic/tiles/purchase_tiles.py
+++ buisness_logic/tiles/purchase_tiles.py
@@ -1,5 +1,5 @@
 from buisness_logic.effects import purchase_effects, resource_effects
-from core.enums.caverna_enums import ResourceTypeEnum, TileTypeEnum
+from core.enums.caverna_enums import ResourceTypeEnum
 from core.baseClasses.base_tile import BaseTile
 from common.entities import weapon
 
@@ -9,7 +9,7 @@ class CarpenterTile(BaseTile):
         super().__init__(
             "Carpenter", 12,
             cost={ResourceTypeEnum.stone: 1},
-            effects=[purchase_effects.DecreasePrice({ResourceTypeEnum.wood: 1}, BaseTile)])
+            effects=[purchase_effects.DecreasePrice(BaseTile, {ResourceTypeEnum.wood: 1})])
 
 
 class StoneCarverTile(BaseTile):
@@ -19,22 +19,30 @@ class StoneCarverTile(BaseTile):
             cost={ResourceTypeEnum.wood: 1},
             effects=[
                 resource_effects.Receive({ResourceTypeEnum.stone: 2}),
-                purchase_effects.DecreasePrice({ResourceTypeEnum.stone: 1}, BaseTile)])
+                purchase_effects.DecreasePrice(BaseTile, {ResourceTypeEnum.stone: 1})])
 
 
 class BlacksmithTile(BaseTile):
     def __init__(self):
-        super().__init__(
-            "Blacksmith", 14, base_points=3,
+        BaseTile.__init__(
+            self, "Blacksmith", 14, base_points=3,
             cost={ResourceTypeEnum.wood: 1, ResourceTypeEnum.stone: 2},
             effects=[
                 resource_effects.Receive({ResourceTypeEnum.ore: 2}),
-                purchase_effects.DecreasePrice({ResourceTypeEnum.ore: 2}, weapon.Weapon)])
+                purchase_effects.DecreasePrice(weapon.Weapon, {ResourceTypeEnum.ore: 2})])
 
 
 class BuilderTile(BaseTile):
     def __init__(self):
-        super().__init__(
-            "Builder", 16, base_points=2,
+        BaseTile.__init__(
+            self, "Builder", 16, base_points=2,
             cost={ResourceTypeEnum.stone: 1},
-            effects=[purchase_effects.DecreasePrice({ResourceTypeEnum.ore: 2}, weapon.Weapon)])
+            effects=[
+                purchase_effects.AllowSubstitutionForPurchase(
+                    BaseTile,
+                    substitute_for={ResourceTypeEnum.wood: 1},
+                    substitute_with={ResourceTypeEnum.ore: 1}),
+                purchase_effects.AllowSubstitutionForPurchase(
+                    BaseTile,
+                    substitute_for={ResourceTypeEnum.stone: 1},
+                    substitute_with={ResourceTypeEnum.ore: 1})])
