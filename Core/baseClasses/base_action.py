dd065182a43f69eb214a1f3a89c89306f7d13d89
Alastair Codd <alastaircodd@hotmail.co.uk>
2019-08-27 09:43:29 +0100

    Change signature on base_action to match implementation

--- Core/baseClasses/base_action.py
+++ Core/baseClasses/base_action.py
@@ -1,12 +1,13 @@
 from abc import abstractmethod, ABC
 
+from common.entities.dwarf import Dwarf
 from core.containers.resource_container import ResourceContainer
 
 
 class BaseAction(ABC):
 
     @abstractmethod
-    def invoke(self, player: 'Player', active_card: ResourceContainer) -> bool:
+    def invoke(self, player: 'Player', active_card: ResourceContainer, current_dwarf: Dwarf) -> bool:
         raise NotImplementedError("abstract base action class")
 
     @abstractmethod
