7f50ea20fa2558cab1a79a1861f49192af4c266f
Alastair Codd <alastaircodd@hotmail.co.uk>
2019-08-27 09:43:13 +0100

    Add missing abstract methods to player

--- Common/Entities/player.py
+++ Common/Entities/player.py
@@ -1,6 +1,8 @@
 from abc import abstractmethod, ABC
-from typing import Iterable, List, Dict
+from typing import Iterable, List, Dict, Union
 from common.entities.dwarf import Dwarf
+from common.entities.weapon import Weapon
+from core.baseClasses.base_tile import BaseTile
 from core.containers.resource_container import ResourceContainer
 from core.containers.tile_container import TileContainer
 from core.enums.caverna_enums import ResourceTypeEnum
@@ -53,6 +55,44 @@ class Player(TileContainer, ResourceContainer, ABC):
         raise NotImplementedError()
 
     @abstractmethod
-    def get_player_choice_market_action(self, possible_items_and_costs: Dict[ResourceTypeEnum, int]) \
+    def get_player_choice_market_action(
+            self,
+            possible_items_and_costs: Dict[ResourceTypeEnum, int]) \
             -> List[ResourceTypeEnum]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_player_choice_weapon_level(self) -> int:
+        """Gets user choice for how strong a weapon to give to the current dwarf."""
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_player_choice_breed_animals(
+            self,
+            animals_which_can_reproduce: List[ResourceTypeEnum],
+            possible_number_of_animals_to_reproduce: int) \
+            -> List[ResourceTypeEnum]:
+        """Get user choice for which animals to breed.
+
+        :param animals_which_can_reproduce: List of possible animals which can reproduce at the moment.
+            This cannot be null.
+        :param possible_number_of_animals_to_reproduce: Number of animals to choose.
+            This will be in the range [0, len(animals_which_can_reproduce)].
+        :returns: A list of animals which will be bred this turn. This will never be null, and length will match
+            possible_number_of_animals_to_reproduce.
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_player_choice_discount(
+            self,
+            possible_prices: List[Dict[ResourceTypeEnum, int]],
+            target: Union[BaseTile, Weapon]) \
+            -> Dict[ResourceTypeEnum, int]:
+        """Gets user choice for which discount level to apply.
+
+        :param possible_prices: A list of possible costs for some item (either a tile or a weapon). This cannot be null.
+        :param target: The item which is to be purchased. This cannot be null.
+        :returns: The chosen cost of the item. This will never be null, and will be an element of possible_prices.
+        """
         raise NotImplementedError()
