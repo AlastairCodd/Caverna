55d27db59418b002b97790b9951a91985aa03a84
Alastair Codd <Alastair.Codd@abdynamics.com>
2019-09-06 17:13:10 +0100

    Relax params on argument out of range error

--- core/exceptions/ArgumentOutOfRangeError.py
+++ core/exceptions/ArgumentOutOfRangeError.py
@@ -1,3 +1,7 @@
 class ArgumentOutOfRangeError(Exception):
-    def __init__(self, length_one: str, length_two: str) -> None:
+    def __init__(self, length_one: str = "", length_two: str = "") -> None:
+        if length_one.isspace():
+            self.message = "Argument is outside range"
+        if length_two.isspace():
+            self.message = f"{length_one} is outside range."
         self.message = f"{length_one} is not equal to {length_two}"
