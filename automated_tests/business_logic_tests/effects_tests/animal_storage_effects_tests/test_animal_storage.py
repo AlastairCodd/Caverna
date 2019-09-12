c91bb45363d514f8be5abd6a5df047c37e057a2c
Alastair Codd <alastaircodd@hotmail.co.uk>
2019-09-12 23:15:12 +0100

    Get success failure logging working

--- automated_tests/business_logic_tests/effects_tests/animal_storage_effects_tests/test_animal_storage.py
+++ automated_tests/business_logic_tests/effects_tests/animal_storage_effects_tests/test_animal_storage.py
@@ -1,5 +1,5 @@
 from math import floor
-from typing import List, Generator, Union, Dict, Iterable
+from typing import List, Generator, Union, Dict, Iterable, Tuple
 from buisness_logic.validators.partition_resource_validator import PartitionResourceValidator
 from common.forges.integer_partition_forge import IntegerPartitionForge
 from common.services.integer_partition_permutation_forge import IntegerPartitionPermutationForge
@@ -30,21 +30,54 @@ class Investigation():
         number_of_resource_possibilities: int = (max_for_resource + 1) ** len(self._animals)
         print(number_of_resource_possibilities)
         print()
-        for i in range(number_of_resource_possibilities):
+        for i in range(1, number_of_resource_possibilities):
             resources_per_animal: Dict[ResourceTypeEnum, int] = {
                 ResourceTypeEnum.sheep: floor((i / (max_for_resource + 1) ** 0)) % (max_for_resource + 1),
                 ResourceTypeEnum.boar: floor((i / (max_for_resource + 1) ** 1)) % (max_for_resource + 1),
                 ResourceTypeEnum.donkey: floor((i / (max_for_resource + 1) ** 2)) % (max_for_resource + 1),
                 ResourceTypeEnum.cow: floor((i / (max_for_resource + 1) ** 3)) % (max_for_resource + 1)
             }
+            animal: ResourceTypeEnum
+            amount: int
+            for (animal, amount) in resources_per_animal.items():
+                print(f"{animal.name}:\t{amount}")
+            print()
+
             for resource_layout in self.generate_resource_layouts(resources_per_animal, max_per_tile, number_of_tiles):
-                successful_partitions: List[List[Union[ResourceTypeEnum, None]]] = \
-                   self.check_resource_layout_against_possible_set_partitions(resource_layout, {})
+                print(" --- new resource layout ---")
+                for animal in self._animals:
+                    print(f"{animal.name}:\t", end="")
+                    for tile in resource_layout:
+                        print(f"{tile[animal]}\t", end="")
+                    print()
+
+                successful_partitions: List[Tuple[List[Union[ResourceTypeEnum, None]], Dict[ResourceTypeEnum, int]]] = \
+                    list(self.check_resource_layout_against_possible_set_partitions(resource_layout, resources_per_animal))
+                if any(successful_partitions):
+                    for (partition, excess) in successful_partitions:
+                        tile: Union[ResourceTypeEnum, None]
+                        for tile in partition:
+                            if tile is None:
+                                print("_\t\t", end="")
+                            else:
+                                if tile is ResourceTypeEnum.cow:
+                                    print(f"cow\t\t", end="")
+                                else:
+                                    print(f"{tile.name}\t", end="")
+                        print(f"excess: {{", end="")
+                        print(f"sheep: {excess[ResourceTypeEnum.sheep]}, ", end="")
+                        print(f"boar: {excess[ResourceTypeEnum.boar]}, ", end="")
+                        print(f"donkey: {excess[ResourceTypeEnum.donkey]}, ", end=""),
+                        print(f"cow: {excess[ResourceTypeEnum.cow]}}}")
+                else:
+                    print("no successful allocations")
+                print()
 
     def check_resource_layout_against_possible_set_partitions(
             self,
             resource_layout: List[Dict[ResourceTypeEnum, int]],
-            current_resources: Dict[ResourceTypeEnum, int]) -> List[List[Union[ResourceTypeEnum, None]]]:
+            current_resources: Dict[ResourceTypeEnum, int]) \
+            -> Iterable[Tuple[List[Union[ResourceTypeEnum, None]], Dict[ResourceTypeEnum, int]]]:
         """Checks the provided resource layout against all possible set partitions,
         and returns the ones which are able to store the provided resources.
 
@@ -53,11 +86,10 @@ class Investigation():
         :returns: A list containing all set partitions (list with same length as resource layout, with entries matching which resource type holds this
         partition. This will never be null, but may be empty.
         """
-        successes: List[List[Union[ResourceTypeEnum, None]]] = []
         for partition in self.generate_set_partitions(len(resource_layout)):
-            if partition is self._partitionResourceValidator.does_partition_store_all_resources(resource_layout, current_resources, partition):
-                successes.append(partition)
-        return successes
+            excess = self._partitionResourceValidator.get_resource_excess(resource_layout, current_resources, partition)
+            if all([x == 0 for x in excess.values()]):
+                yield (partition, excess)
 
     def generate_resource_layouts(
             self,
@@ -65,7 +97,7 @@ class Investigation():
             max_resources_per_tile: int,
             number_of_tiles: int) \
             -> Generator[List[Dict[ResourceTypeEnum, int]], None, None]:
-        sheep_partitions: Iterable[List[int]] = self._integerPartitionForge\
+        sheep_partitions: Iterable[List[int]] = self._integerPartitionForge \
             .generate_integer_partitions(resources_per_animal[ResourceTypeEnum.sheep])
         for sheep_partition in sheep_partitions:
             if not self._are_partition_dimensions_valid(sheep_partition, max_resources_per_tile, number_of_tiles):
@@ -74,7 +106,7 @@ class Investigation():
                 .generate_permutation(sheep_partition, number_of_tiles)
             for permuted_sheep_partition in permuted_sheep_partitions:
 
-                boar_partitions: Iterable[List[int]] = self._integerPartitionForge\
+                boar_partitions: Iterable[List[int]] = self._integerPartitionForge \
                     .generate_integer_partitions(resources_per_animal[ResourceTypeEnum.boar])
                 for boar_partition in boar_partitions:
                     if not self._are_partition_dimensions_valid(boar_partition, max_resources_per_tile, number_of_tiles):
@@ -83,7 +115,7 @@ class Investigation():
                         .generate_permutation(boar_partition, number_of_tiles)
                     for permuted_boar_partition in permuted_boar_partitions:
 
-                        donkey_partitions: Iterable[List[int]] = self._integerPartitionForge\
+                        donkey_partitions: Iterable[List[int]] = self._integerPartitionForge \
                             .generate_integer_partitions(resources_per_animal[ResourceTypeEnum.donkey])
                         for donkey_partition in donkey_partitions:
                             if not self._are_partition_dimensions_valid(donkey_partition, max_resources_per_tile, number_of_tiles):
@@ -92,7 +124,7 @@ class Investigation():
                                 .generate_permutation(donkey_partition, number_of_tiles)
                             for permuted_donkey_partition in permuted_donkey_partitions:
 
-                                cow_partitions: Iterable[List[int]] = self._integerPartitionForge\
+                                cow_partitions: Iterable[List[int]] = self._integerPartitionForge \
                                     .generate_integer_partitions(resources_per_animal[ResourceTypeEnum.cow])
                                 for cow_partition in cow_partitions:
                                     if not self._are_partition_dimensions_valid(cow_partition, max_resources_per_tile, number_of_tiles):
--- buisness_logic/validators/partition_resource_validator.py
+++ buisness_logic/validators/partition_resource_validator.py
@@ -20,16 +20,32 @@ class PartitionResourceValidator:
         if len(partition) != len(resource_layout):
             raise ArgumentOutOfRangeError("partition", "resource layout")
 
+        remaining_resources: Dict[T, int] = self.get_resource_excess(resource_layout, current_resources, partition)
+        result: bool = all([x == 0 for x in remaining_resources.values()])
+        return result
+
+    def get_resource_excess(
+            self,
+            resource_layout: List[Dict[T, int]],
+            current_resources: Dict[T, int],
+            partition: List[Union[T, None]]) -> Dict[T, int]:
+        if resource_layout is None:
+            raise ValueError("resource layout may not be null")
+        if current_resources is None:
+            raise ValueError("current resources may not be null")
+        if partition is None:
+            raise ValueError("partition may not be null")
+        if len(partition) != len(resource_layout):
+            raise ArgumentOutOfRangeError("partition", "resource layout")
+
         remaining_resources: Dict[T, int] = dict(current_resources)
         for i in range(len(partition)):
             object_stored_in_current: Union[T, None] = partition[i]
             if object_stored_in_current is not None:
-                amount_of_resources_allowed_in_current: int = \
-                    resource_layout[i][object_stored_in_current]
-                if remaining_resources[object_stored_in_current] < amount_of_resources_allowed_in_current:
-                    remaining_resources[object_stored_in_current] = 0
-                else:
-                    remaining_resources[object_stored_in_current] -= amount_of_resources_allowed_in_current
-
-        result: bool = all([x == 0 for x in remaining_resources.values()])
-        return result
+                amount_of_resources_allowed_in_current: int = resource_layout[i][object_stored_in_current]
+                if object_stored_in_current in remaining_resources:
+                    if remaining_resources[object_stored_in_current] < amount_of_resources_allowed_in_current:
+                        remaining_resources[object_stored_in_current] = 0
+                    else:
+                        remaining_resources[object_stored_in_current] -= amount_of_resources_allowed_in_current
+        return remaining_resources
