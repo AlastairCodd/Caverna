0cacf98eac439d8572bdaff69076511e0007fca4
Alastair Codd <alastaircodd@hotmail.co.uk>
2019-11-05 08:34:27 +0000

    Resource Container Prototype

--- common/prototypes/resource_container_prototype.py
+++ common/prototypes/resource_container_prototype.py
@@ -1,2 +1,23 @@
-class ResourceContainerPrototype(object):
+from core.baseClasses.base_prototype import BasePrototype, T
+from core.containers.resource_container import ResourceContainer
+
+
+class ResourceContainerPrototype(BasePrototype[ResourceContainer]):
     def __init__(self):
+        pass
+
+    def clone(self, source: ResourceContainer) -> ResourceContainer:
+        if source is None:
+            raise ValueError
+
+        target = ResourceContainer()
+        self.assign(source, target)
+        return target
+
+    def assign(self, source: ResourceContainer, target: ResourceContainer) -> None:
+        if source is None:
+            raise ValueError
+        if target is None:
+            raise ValueError
+
+        target.give_resources(source.resources)
