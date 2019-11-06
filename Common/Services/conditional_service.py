37f07cafbb8dc104a3b164841c7c41197722b3d5
Alastair Codd <Alastair.Codd@abdynamics.com>
2019-11-06 16:34:08 +0000

    Revert conditional service to not use static methods

--- Common/Services/conditional_service.py
+++ Common/Services/conditional_service.py
@@ -6,144 +6,6 @@ from core.enums.caverna_enums import ActionCombinationEnum
 from common.entities.multiconditional import Conditional
 
 
-def _combine_and_then(left: Iterable[List[BaseAction]], right: Iterable[List[BaseAction]]) \
-        -> List[List[BaseAction]]:
-    """Combine the left and right lists in an and then way. (left or right)
-    a AND_THEN b = [ab]
-
-    params:
-        left: an enumerable of base actions. This cannot be null.
-        right: an enumerable of base actions. This cannot be null.
-
-    returns:
-        a list containing the possible combined actions. This will never be null."""
-    if left is None:
-        raise ValueError("left")
-    if right is None:
-        raise ValueError("right")
-
-    result: List[List[BaseAction]] = []
-
-    for l in left:
-        for r in right:
-            result.append(l + r)
-
-    return result
-
-
-def _combine_or(left: Iterable[List[BaseAction]], right: Iterable[List[BaseAction]]) \
-        -> List[List[BaseAction]]:
-    """Combine the left and right lists in an and then way. (left or right)
-    a OR b = [a, b]
-
-    params:
-        left: an enumerable of base actions. This cannot be null.
-        right: an enumerable of base actions. This cannot be null.
-
-    returns:
-        a list containing the possible combined actions. This will never be null."""
-    if left is None:
-        raise ValueError("left")
-    if right is None:
-        raise ValueError("right")
-
-    result: List[List[BaseAction]] = []
-
-    for l in left:
-        result.append(l)
-
-    for r in right:
-        result.append(r)
-
-    return result
-
-
-def _combine_and_then_or(left: Iterable[List[BaseAction]], right: Iterable[List[BaseAction]]) \
-        -> List[List[BaseAction]]:
-    """Combine the left and right lists in an and then way. (left or right)
-    a AND_THEN_OR b = [ab, b]
-
-    params:
-        left: an enumerable of base actions. This cannot be null.
-        right: an enumerable of base actions. This cannot be null.
-
-    returns:
-        a list containing the possible combined actions. This will never be null."""
-    if left is None:
-        raise ValueError("left")
-    if right is None:
-        raise ValueError("right")
-
-    result: List[List[BaseAction]] = []
-
-    for l in left:
-        for r in right:
-            result.append(l + r)
-
-    for r in right:
-        result.append(r)
-
-    return result
-
-
-def _combine_and_or(left: Iterable[List[BaseAction]], right: Iterable[List[BaseAction]]) \
-        -> List[List[BaseAction]]:
-    """Combine the left and right lists in an and way. (left or right or left and right)
-    a AND_OR b = [a, b, ab]
-
-    params:
-        left: an enumerable of base actions. This cannot be null.
-        right: an enumerable of base actions. This cannot be null.
-
-    returns:
-        a list containing the possible combined actions. This will never be null."""
-    if left is None:
-        raise ValueError("left")
-    if right is None:
-        raise ValueError("right")
-
-    result: List[List[BaseAction]] = []
-
-    for l in left:
-        result.append(l)
-
-    for r in right:
-        result.append(r)
-
-    for l in left:
-        for r in right:
-            result.append(l + r)
-
-    return result
-
-
-def _combine_either_or(left: Iterable[List[BaseAction]], right: Iterable[List[BaseAction]]) \
-        -> List[List[BaseAction]]:
-    """Combine the left and right lists in an either or way. (left or right)
-    a EITHER_OR b = [a,b]
-
-    params:
-        left: an enumerable of base actions. This cannot be null.
-        right: an enumerable of base actions. This cannot be null.
-
-    returns:
-        a list containing the possible combined actions. This will never be null."""
-    if left is None:
-        raise ValueError("left")
-    if right is None:
-        raise ValueError("right")
-
-    result: List[List[BaseAction]] = []
-
-    for l in left:
-        result.append(l)
-
-    for r in right:
-        result.append(r)
-
-    return result
-
-
 class ConditionalService(object):
 
     def __init__(self):
@@ -153,11 +15,11 @@ class ConditionalService(object):
             Callable[
                 [List[List[BaseAction]], List[List[BaseAction]]],
                 List[List[BaseAction]]]] = {
-            ActionCombinationEnum.EitherOr: _combine_either_or,
-            ActionCombinationEnum.AndOr: _combine_and_or,
-            ActionCombinationEnum.AndThenOr: _combine_and_then_or,
-            ActionCombinationEnum.Or: _combine_or,
-            ActionCombinationEnum.AndThen: _combine_and_then}
+            ActionCombinationEnum.EitherOr: self._combine_either_or,
+            ActionCombinationEnum.AndOr: self._combine_and_or,
+            ActionCombinationEnum.AndThenOr: self._combine_and_then_or,
+            ActionCombinationEnum.Or: self._combine_or,
+            ActionCombinationEnum.AndThen: self._combine_and_then}
 
     def get_possible_choices(
             self,
@@ -190,3 +52,136 @@ class ConditionalService(object):
 
         choices: List[List[BaseAction]] = self._actionDictionary[combination_type](left, right)
         return choices
+
+    def _combine_and_then(self, left: Iterable[List[BaseAction]], right: Iterable[List[BaseAction]]) \
+            -> List[List[BaseAction]]:
+        """Combine the left and right lists in an and then way. (left or right)
+        a AND_THEN b = [ab]
+
+        params:
+            left: an enumerable of base actions. This cannot be null.
+            right: an enumerable of base actions. This cannot be null.
+
+        returns:
+            a list containing the possible combined actions. This will never be null."""
+        if left is None:
+            raise ValueError("left")
+        if right is None:
+            raise ValueError("right")
+
+        result: List[List[BaseAction]] = []
+
+        for l in left:
+            for r in right:
+                result.append(l + r)
+
+        return result
+
+    def _combine_or(self, left: Iterable[List[BaseAction]], right: Iterable[List[BaseAction]]) \
+            -> List[List[BaseAction]]:
+        """Combine the left and right lists in an and then way. (left or right)
+        a OR b = [a, b]
+
+        params:
+            left: an enumerable of base actions. This cannot be null.
+            right: an enumerable of base actions. This cannot be null.
+
+        returns:
+            a list containing the possible combined actions. This will never be null."""
+        if left is None:
+            raise ValueError("left")
+        if right is None:
+            raise ValueError("right")
+
+        result: List[List[BaseAction]] = []
+
+        for l in left:
+            result.append(l)
+
+        for r in right:
+            result.append(r)
+
+        return result
+
+    def _combine_and_then_or(self, left: Iterable[List[BaseAction]], right: Iterable[List[BaseAction]]) \
+            -> List[List[BaseAction]]:
+        """Combine the left and right lists in an and then way. (left or right)
+        a AND_THEN_OR b = [ab, b]
+
+        params:
+            left: an enumerable of base actions. This cannot be null.
+            right: an enumerable of base actions. This cannot be null.
+
+        returns:
+            a list containing the possible combined actions. This will never be null."""
+        if left is None:
+            raise ValueError("left")
+        if right is None:
+            raise ValueError("right")
+
+        result: List[List[BaseAction]] = []
+
+        for l in left:
+            for r in right:
+                result.append(l + r)
+
+        for r in right:
+            result.append(r)
+
+        return result
+
+    def _combine_and_or(self, left: Iterable[List[BaseAction]], right: Iterable[List[BaseAction]]) \
+            -> List[List[BaseAction]]:
+        """Combine the left and right lists in an and way. (left or right or left and right)
+        a AND_OR b = [a, b, ab]
+
+        params:
+            left: an enumerable of base actions. This cannot be null.
+            right: an enumerable of base actions. This cannot be null.
+
+        returns:
+            a list containing the possible combined actions. This will never be null."""
+        if left is None:
+            raise ValueError("left")
+        if right is None:
+            raise ValueError("right")
+
+        result: List[List[BaseAction]] = []
+
+        for l in left:
+            result.append(l)
+
+        for r in right:
+            result.append(r)
+
+        for l in left:
+            for r in right:
+                result.append(l + r)
+
+        return result
+
+    def _combine_either_or(self, left: Iterable[List[BaseAction]], right: Iterable[List[BaseAction]]) \
+            -> List[List[BaseAction]]:
+        """Combine the left and right lists in an either or way. (left or right)
+        a EITHER_OR b = [a,b]
+
+        params:
+            left: an enumerable of base actions. This cannot be null.
+            right: an enumerable of base actions. This cannot be null.
+
+        returns:
+            a list containing the possible combined actions. This will never be null."""
+        if left is None:
+            raise ValueError("left")
+        if right is None:
+            raise ValueError("right")
+
+        result: List[List[BaseAction]] = []
+
+        for l in left:
+            result.append(l)
+
+        for r in right:
+            result.append(r)
+
+        return result
