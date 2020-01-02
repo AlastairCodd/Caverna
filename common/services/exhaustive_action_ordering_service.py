f96aee8870339d120d0261260352df1fa037b8b3
Alastair Codd <alastaircodd@hotmail.co.uk>
2020-01-02 12:48:00 +0000

    Use Permutation ordering service in Exhaustive Action Ordering Service

--- common/services/exhaustive_action_ordering_service.py
+++ common/services/exhaustive_action_ordering_service.py
@@ -1,5 +1,7 @@
-from typing import List, Dict, Iterable, Tuple
+from typing import List, Iterable, Tuple, Union
 
+from buisness_logic.services.most_points_permutation_ordering_service import MostPointsPermutationOrderingService
+from buisness_logic.services.most_resources_permutation_ordering_service import MostResourcesPermutationOrderingService
 from common.entities.action_choice_lookup import ActionChoiceLookup
 from common.entities.dwarf import Dwarf
 from common.entities.player import Player
@@ -8,20 +10,22 @@ from common.prototypes.player_prototype import PlayerPrototype
 from core.baseClasses.base_action import BaseAction
 from core.baseClasses.base_action_ordering_service import ActionOrderingService
 from core.baseClasses.base_card import BaseCard
-from core.containers.resource_container import ResourceContainer
-from core.enums.caverna_enums import ResourceTypeEnum
+from core.baseClasses.base_permutation_ordering_service import BasePermutationOrderingService
 
 
 class ExhaustiveActionOrderingService(ActionOrderingService):
     def __init__(self):
         self._player_prototype: PlayerPrototype = PlayerPrototype()
+        self._permutation_ordering_services: List[BasePermutationOrderingService] = \
+            [MostPointsPermutationOrderingService(),
+             MostResourcesPermutationOrderingService()]
 
     def calculated_best_order(
             self,
             actions: ActionChoiceLookup,
             player: Player,
             current_card: BaseCard,
-            current_dwarf: Dwarf) -> List[BaseAction]:
+            current_dwarf: Dwarf) -> ResultLookup[List[BaseAction]]:
         if actions is None:
             raise ValueError
         if player is None:
@@ -31,8 +35,10 @@ class ExhaustiveActionOrderingService(ActionOrderingService):
         if current_dwarf is None:
             raise ValueError
 
-        successful_permutations: List[Tuple[int, ResourceContainer]] =[]
+        successful_permutations: List[Tuple[List[BaseAction], int, Player]] = []
         unsuccessful_permutations: List[ResultLookup[int]] = []
+
+        permutation: List[BaseAction]
 
         for permutation in self._get_permutations(actions.actions):
             if all(constraint.passes_condition(permutation) for constraint in actions.constraints):
@@ -58,13 +64,36 @@ class ExhaustiveActionOrderingService(ActionOrderingService):
 
                 permutation_result: ResultLookup[int] = ResultLookup(success, successes, errors_for_permutation)
                 if success:
-                    success_result: Tuple[int, ResourceContainer] = (successes, player_for_permutation)
+                    success_result: Tuple[List[BaseAction], int, Player] = (permutation, successes, player_for_permutation)
                     successful_permutations.append(success_result)
                 else:
                     unsuccessful_permutations.append(permutation_result)
 
-        if any(successful_permutations):
-            pass
+        result: Union[ResultLookup[List[BaseAction]], None] = None
+        permutation_ordering_service_index: int = 0
+        permutation_ordering_service: BasePermutationOrderingService
+        ranked_results: List[Tuple[List[BaseAction], int, Player]] = successful_permutations
 
-        score: Dict[List[BaseAction], Dict[ResourceTypeEnum, int]] = {}
-        raise NotImplementedError
+        if any(ranked_results):
+            while result is None:
+                if permutation_ordering_service_index >= len(self._permutation_ordering_services):
+                    permutation_ordering_service = self._permutation_ordering_services[permutation_ordering_service_index]
+                    ordering_result = permutation_ordering_service.find_best_permutation(ranked_results)
+
+                    if ordering_result.flag:
+                        result = ResultLookup(True, ordering_result.value[0][0])
+                    elif any(ordering_result.value):
+                        permutation_ordering_service_index += 1
+                        ranked_results = ordering_result.value
+                    else:
+                        result = ResultLookup(errors=ordering_result.errors)
+                else:
+                    # if we've exhausted all ordering services, just pick a permutation at random
+                    result = ResultLookup(True, ranked_results[0][0])
+        else:
+            result = ResultLookup(errors="There is not a permutation which allows for all actions to be performed")
+        return result
+
+    def _get_permutations(self, actions: List[BaseAction]) -> Iterable[List[BaseAction]]:
+        if actions is None:
+            raise ValueError
