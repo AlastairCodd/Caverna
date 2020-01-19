a022efffa5096fc8f6c6a7a1f523e4db6a3a8064
Alastair Codd <alastaircodd@hotmail.co.uk>
2020-01-19 17:25:42 +0000

    More work on tests

--- automated_tests/common_tests/service_tests/exhaustive_action_ordering_service_tests/calculated_best_order_tests/test_when_called_with_single_highest_score_and_no_constraints.py
+++ automated_tests/common_tests/service_tests/exhaustive_action_ordering_service_tests/calculated_best_order_tests/test_when_called_with_single_highest_score_and_no_constraints.py
@@ -2,18 +2,20 @@ from typing import List, Dict
 
 from automated_tests.common_tests.service_tests.exhaustive_action_ordering_service_tests.given_an_exhaustive_action_ordering_service import \
     Given_An_ExhaustiveActionOrderingService
+from automated_tests.mocks.MockCard import MockCard
 from automated_tests.mocks.mock_player import MockPlayer
 from buisness_logic.actions.giveDwarfAWeaponAction import GiveDwarfAWeaponAction
 from buisness_logic.actions.goOnAnExpeditionAction import GoOnAnExpeditionAction
 from buisness_logic.actions.takeAccumulatedItemsAction import TakeAccumulatedItemsAction
 from common.entities.action_choice_lookup import ActionChoiceLookup
 from common.entities.dwarf import Dwarf
+from common.entities.multiconditional import Conditional
 from common.entities.player import Player
 from common.entities.result_lookup import ResultLookup
 from core.baseClasses.base_action import BaseAction
 from core.baseClasses.base_card import BaseCard
 from core.baseClasses.base_constraint import BaseConstraint
-from core.enums.caverna_enums import ResourceTypeEnum
+from core.enums.caverna_enums import ResourceTypeEnum, ActionCombinationEnum
 
 
 class Test_When_Called_With_Single_Highest_Score_And_No_Constraints(Given_An_ExhaustiveActionOrderingService):
@@ -22,7 +24,7 @@ class Test_When_Called_With_Single_Highest_Score_And_No_Constraints(Given_An_Exh
         action2: BaseAction = GiveDwarfAWeaponAction()
         action3: BaseAction = GoOnAnExpeditionAction(level=2)
 
-        actions: List[BaseAction] = []
+        actions: List[BaseAction] = [action1, action2, action3]
         constraints: List[BaseConstraint] = []
         action_choice_lookup: ActionChoiceLookup = ActionChoiceLookup(actions, constraints)
 
@@ -31,8 +33,9 @@ class Test_When_Called_With_Single_Highest_Score_And_No_Constraints(Given_An_Exh
         resources: Dict[ResourceTypeEnum, int] = {}
 
         player: Player = MockPlayer(3,resources=resources)
-        current_card: BaseCard = MockCard(resources={ResourceTypeEnum.ore: 3})
-        current_dwarf: Dwarf = MockDwarf()
+        action_conditional: Conditional = Conditional(ActionCombinationEnum.AndOr, action1, Conditional(ActionCombinationEnum.AndOr, action2, action3))
+        current_card: BaseCard = MockCard(actions=action_conditional, resources={ResourceTypeEnum.ore: 3})
+        current_dwarf: Dwarf = Dwarf()
 
         self._result: ResultLookup[List[BaseAction]] = self.SUT.calculated_best_order(
             action_choice_lookup,
