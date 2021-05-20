bdedec176c3a593f2419e0bc274e0169ba8f1106
Alastair Codd <alastaircodd@hotmail.co.uk>
2021-05-20 23:02:17 +0100

    Extract default game dimension to constants
    (cherry picked from commit 7815318a778841353c71725a405f958a345a480e)

--- core/constants/game_constants.py
+++ core/constants/game_constants.py
@@ -2,3 +2,7 @@ number_of_rounds: int = 12
 soft_maximum_number_of_dwarves: int = 5
 maximum_number_of_dwarves: int = 6
 maximum_number_of_stables: int = 3
+
+default_board_width: int = 8
+default_board_height: int = 6
+default_board_tile_count: int = default_board_width * default_board_height
