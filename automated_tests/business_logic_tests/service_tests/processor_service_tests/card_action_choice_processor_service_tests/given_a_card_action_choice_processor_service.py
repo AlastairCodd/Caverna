16053fd24952e9e8929d7e7c04c67ac4563824ed
Alastair Codd <alastaircodd@hotmail.co.uk>
2021-06-09 22:29:30 +0100

    Tests: Add tests for Card and Dwarf Choice Processor

---
+++ automated_tests/business_logic_tests/service_tests/processor_service_tests/card_action_choice_processor_service_tests/given_a_card_action_choice_processor_service.py
@@ -0,0 +1,14 @@
+from abc import ABCMeta
+from unittest import TestCase
+
+from buisness_logic.services.processor_services.weapon_level_action_choice_processor_service import CardActionChoiceProcessorService
+
+
+# noinspection PyPep8Naming
+class Given_A_CardActionChoiceProcessorService(TestCase, metaclass=ABCMeta):
+    def setUp(self) -> None:
+        self.SUT: CardActionChoiceProcessorService = CardActionChoiceProcessorService()
+        self.because()
+
+    def because(self) -> None:
+        pass
