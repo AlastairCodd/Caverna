9053ca2474f365904357e16256ce91ee2b79d9d5
Alastair Codd <alastaircodd@hotmail.co.uk>
2021-06-23 23:23:39 +0100

    Tests: Fix test package name

--- automated_tests/business_logic_tests/service_tests/processor_service_tests/expedition_reward_action_choice_processor_service_tests/process_action_choice_for_rewards_tests/test_when_some_choices_are_invalid.py
+++ automated_tests/business_logic_tests/service_tests/processor_service_tests/expedition_reward_action_choice_processor_service_tests/process_action_choice_for_rewards_tests/test_when_some_choices_are_invalid.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from automated_tests.business_logic_tests.service_tests.processor_service_tests.expedition_reward_action_choice_processor_service.given_an_expedition_reward_action_choice_processor_service import \
+from automated_tests.business_logic_tests.service_tests.processor_service_tests.expedition_reward_action_choice_processor_service_tests.given_an_expedition_reward_action_choice_processor_service import \
     Given_An_ExpeditionRewardActionChoiceProcessorService
 from buisness_logic.actions.place_a_single_tile_action import PlaceASingleTileAction
 from buisness_logic.actions.place_a_twin_tile_action import PlaceATwinTileAction
@@ -11,7 +11,7 @@ from core.baseClasses.base_action import BaseAction
 from core.enums.caverna_enums import ResourceTypeEnum, TileTypeEnum
 
 
-class test_when_is_first_expedition(Given_An_ExpeditionRewardActionChoiceProcessorService):
+class test_when_some_choices_are_invalid(Given_An_ExpeditionRewardActionChoiceProcessorService):
     def because(self) -> None:
         number_of_possible_rewards: int = 23
         self._expedition_level: int = 3
@@ -19,7 +19,7 @@ class test_when_is_first_expedition(Given_An_ExpeditionRewardActionChoiceProcess
         possible_actions: List[Optional[BaseAction]] = [None for _ in range(number_of_possible_rewards)]
         action_choice: List[float] = [0.0 for _ in range(2 * number_of_possible_rewards)]
 
-        self._expected_indexes: List[int] = [6, 15, 21]
+        self._expected_indexes: List[int] = [6, 8, 14]
         self._expected_rewards: List[BaseAction] = [
             ReceiveAction({ResourceTypeEnum.donkey: 1}),
             PlaceATwinTileAction(TileTypeEnum.pastureTwin, {ResourceTypeEnum.wood: 2}),
@@ -27,17 +27,23 @@ class test_when_is_first_expedition(Given_An_ExpeditionRewardActionChoiceProcess
         ]
 
         chosen_action_weighting: float = 0.8
+        invalid_action_weighting: float = 0.9
         out_of_range_action_weighting: float = 1.0
 
         for index, reward in zip(self._expected_indexes, self._expected_rewards):
             action_choice[index] = chosen_action_weighting
             possible_actions[index] = reward
 
+        action_choice[17] = invalid_action_weighting
+        action_choice[18] = invalid_action_weighting
+        action_choice[21] = invalid_action_weighting
+
         action_choice[2 + number_of_possible_rewards] = out_of_range_action_weighting
         action_choice[16 + number_of_possible_rewards] = out_of_range_action_weighting
         action_choice[17 + number_of_possible_rewards] = out_of_range_action_weighting
 
         self.SUT.set_action_choice(action_choice)
+        self.SUT.mark_invalid_action(18)  # should mark this and all higher?
 
         self._result: ExpeditionRewardActionChoice = self.SUT.process_action_choice_placement_for_rewards(
             self._expedition_level,
