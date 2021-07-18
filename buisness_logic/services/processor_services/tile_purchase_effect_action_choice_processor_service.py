cf2ffa6ffdaec556000fcaf753be4615a0250955
Alastair Codd <alastaircodd@hotmail.co.uk>
2021-07-18 18:18:35 +0100

    ComputerPlayer: Use tile purchase action processor service

---
+++ buisness_logic/services/processor_services/tile_purchase_effect_action_choice_processor_service.py
@@ -0,0 +1,8 @@
+from core.services.base_action_choice_processor_service import BaseActionChoiceProcessorService
+
+
+class TilePurchaseEffectActionChoiceProcessorService(BaseActionChoiceProcessorService):
+    def __init__(self) -> None:
+        number_of_tile_purchase_effects: int = 2
+
+        BaseActionChoiceProcessorService.__init__()
