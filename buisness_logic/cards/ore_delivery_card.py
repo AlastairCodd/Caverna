091ad0830b01dcc7ad656a8eb945201a4b5eeb6e
Lofty Inclination <alastaircodd@hotmail.co.uk>
2023-03-14 23:00:58 +0000

    Cards: Make Ore Delivery Card conditionally give player resources

--- buisness_logic/cards/ore_delivery_card.py
+++ buisness_logic/cards/ore_delivery_card.py
@@ -1,16 +1,23 @@
 from typing import Dict
 
-from buisness_logic.actions import take_accumulated_items_action
+from buisness_logic.actions import receive_conditionally_action, take_accumulated_items_action
+from common.entities.multiconditional import Conditional
 from core.baseClasses.base_resource_containing_card import BaseResourceContainingCard
 from core.constants import card_ids
-from core.enums.caverna_enums import ResourceTypeEnum
+from core.containers.tile_container import TileContainer
+from core.enums.caverna_enums import ActionCombinationEnum, ResourceTypeEnum, TileTypeEnum
+from core.repositories.base_player_repository import BasePlayerRepository
 
 
 class OreDeliveryCard(BaseResourceContainingCard):
     def __init__(self):
         BaseResourceContainingCard.__init__(
             self, "Ore Delivery", card_ids.OreDeliveryCardId, 3,
-            take_accumulated_items_action.TakeAccumulatedItemsAction())
+            Conditional(
+                ActionCombinationEnum.AndThen,
+                take_accumulated_items_action.TakeAccumulatedItemsAction(),
+                receive_conditionally_action.ReceiveConditionallyAction(self._condition, {ResourceTypeEnum.ore: 2})
+        ))
 
     def refill_action(self) -> Dict[ResourceTypeEnum, int]:
         self.give_resources(
@@ -18,3 +25,10 @@ class OreDeliveryCard(BaseResourceContainingCard):
              ResourceTypeEnum.ore: 1})
 
         return self.resources
+
+    def _condition(self, player: BasePlayerRepository) -> int:
+        if player is None:
+            raise ValueError(str(player))
+        tile_container = cast(TileContainer, player)
+        number_of_ore_mines: int = tile_container.get_number_of_tiles_of_type(TileTypeEnum.oreMine)
+        return number_of_ore_mines
