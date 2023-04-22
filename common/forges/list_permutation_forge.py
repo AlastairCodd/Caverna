1669b8d0f819fbef8b29b9a3aa4acbd9d2879c4e
Lofty Inclination <alastaircodd@hotmail.co.uk>
2023-04-22 20:54:26 +0100

    Optimisation+Runner: Do not calculate number of items to permute more than once
    This optimisation completely removes the 3,266,206 calls to len. It's also
    almost entirely pointless, because that only accounted for 0.3s of the
    runtime. Still, it's pretty easy to do.

--- common/forges/list_permutation_forge.py
+++ common/forges/list_permutation_forge.py
@@ -11,20 +11,20 @@ class ListPermutationForge(object):
             list_to_permute: List[T]) -> Generator[List[T], None, None]:
         if list_to_permute is None:
             raise ValueError
-        number_of_permutations: int = math.factorial(len(list_to_permute))
+        number_of_items_to_permute: int = len(list_to_permute)
+        number_of_permutations: int = math.factorial(number_of_items_to_permute)
         for i in range(number_of_permutations):
-            yield self.generate_list_permutation_for_index(list_to_permute, i)
+            yield self.generate_list_permutation_for_index(list_to_permute, i, number_of_items_to_permute)
 
     def generate_list_permutation_for_index(
             self,
             list_to_permute: List[T],
-            index: int) -> List[T]:
+            index: int,
+            number_of_items_to_permute: int) -> List[T]:
         if list_to_permute is None:
             raise ValueError
         if index < 0:
             raise IndexError
-        number_of_items_to_permute = len(list_to_permute)
-
         if number_of_items_to_permute == 0:
             if index == 0:
                 return []
@@ -44,7 +44,7 @@ class ListPermutationForge(object):
         result: List[T] = [list_to_permute[taken_index]]
 
         remaining_list: List[T] = list_to_permute[:taken_index] + list_to_permute[taken_index+1:]
-        permuted_remaining_list: List[T] = self.generate_list_permutation_for_index(remaining_list, remaining_index)
+        permuted_remaining_list: List[T] = self.generate_list_permutation_for_index(remaining_list, remaining_index, number_of_items_to_permute - 1)
 
         result += permuted_remaining_list
         return result
