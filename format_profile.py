ee05c0e749ea374ab2ef8df1bdbab1fce6f99ba7
Lofty Inclination <alastaircodd@hotmail.co.uk>
2023-05-06 23:20:53 +0100

    Meta: Make profile formatting class take file as argument
    I'm tired of changing this around all the time and git wanting to
    check in the change.

--- format_profile.py
+++ format_profile.py
@@ -1,4 +1,15 @@
-import pstats
+if __name__ != "__main__":
+    raise InvalidOperationError("Don't import profile formatter")
 
-p = pstats.Stats("profiles/turn_execution_service_profile.configurable")
-p.sort_stats(pstats.SortKey.TIME).print_stats(.1)
+from argparse import ArgumentParser
+import pstats
+import pathlib
+
+parser = ArgumentParser(prog="Format profile output")
+parser.add_argument('filename', type=pathlib.Path)
+filename = str(parser.parse_args().filename)
+
+pstats.Stats(filename) \
+    .strip_dirs() \
+    .sort_stats(pstats.SortKey.TIME) \
+    .print_stats(.1)
