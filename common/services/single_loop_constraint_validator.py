a40ce9bfbf8c0078a7bcec4c99fdd49c3faec504
Lofty Inclination <alastaircodd@hotmail.co.uk>
2023-05-07 01:47:16 +0100

    Optimisation+Runner: Stop single loop constraint validator from doing equality checks
    The old version of doing this turned out to not be any quicker than
    the collating version, due to cost in:
      * dictionary comprehension,
      * list comprehension, and
      * equality comparison between the current and 2nd action in the
        constraint.
    This new patch gets rid of all of these in favour of calculating if the
    action is the first or second when caching it in the constraints
    dictionary. We also get around the dictionary/list comprehension by
    setting a flag on whether the first action has already been seen,
    instead of removing it from the list. Still not strictly pure, because
    we have to reset these flags afterwards, but finally quicker!
    
    This speeds up the constraint validation from 17.3s to 11.1s, a 36%
    improvement! Finally. It's so late. I'm so tired.

--- common/services/single_loop_constraint_validator.py
+++ common/services/single_loop_constraint_validator.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, NamedTuple
+from typing import List, NamedTuple
 
 from core.baseClasses.base_action import BaseAction
 from core.baseClasses.base_constraint import BaseConstraint
@@ -9,24 +9,41 @@ class SingleLoopConstraintValidator(object):
     def __init__(
             self,
             constraints: List[BaseConstraint]) -> None:
-        self._constraints: Dict[BaseAction, List[BaseConstraint]] = {}
-        for constraint in constraints:
+        self._constraints = {}
+        self._number_of_constraints = len(constraints)
+        self._already_considered: List[bool] = [False for _ in range(self._number_of_constraints)]
+
+        for (i, constraint) in enumerate(constraints):
             if not isinstance(constraint, PrecedesConstraint):
                 raise ValueError("Constraint validator only works for precedes constraints")
             self._constraints.setdefault(constraint._action_one, [])
             self._constraints.setdefault(constraint._action_two, [])
-            self._constraints[constraint._action_one].append(constraint)
-            self._constraints[constraint._action_two].append(constraint)
+
+            self._constraints[constraint._action_one].append((i, True))
+            self._constraints[constraint._action_two].append((i, False))
 
     def get_index_of_first_action_which_fails_constraints(
             self,
             permutation: List[BaseAction]) -> int:
-        constraints = {action: constraints.copy for action, constraints in self._constraints.items()}
+        self._reset()
         for (i, action) in enumerate(permutation):
+            #print()
+            if action not in self._constraints:
+                continue
             constraints_on_action = self._constraints[action]
-            for constraint in constraints_on_action:
-                if constraint._action_two == action:
-                    print(constraint, i)
+            #print(i, "action", repr(action))
+            #print("constraints", constraints_on_action)
+            for (constraint_index, is_first_action) in constraints_on_action:
+                if self._already_considered[constraint_index]:
+                    #print(" ", constraint, "skipped")
+                    continue
+                if not is_first_action:
+                    #print(constraint, i)
                     return i
-                constraints_on_action.remove(constraint)
+                #print(constraint, "good")
+                self._already_considered[constraint_index] = True
         return -1
+
+    def _reset(self) -> None:
+        self._already_considered = [False for _ in range(self._number_of_constraints)]
+
